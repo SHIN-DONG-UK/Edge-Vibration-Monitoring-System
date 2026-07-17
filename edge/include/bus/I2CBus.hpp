a64b7e729479cff38a5c10f1aadcf5200c2faa4b
SHIN-DONG-UK <ehd2610@naver.com>
2026-07-17 14:04:02 +0900

    refactor: Fix syntax error.

--- edge/include/bus/I2CBus.hpp
+++ edge/include/bus/I2CBus.hpp
@@ -3,7 +3,6 @@
 #include <cstddef>
 #include <cstdint>
 #include <string>
-using namespace std;
 
 class I2CBus{
     public:
@@ -13,26 +12,26 @@ class I2CBus{
         ~I2CBus();
 
         I2CBus(const I2CBus&) = delete;             // 복사 생성 금지
-        I2CBus& operator=(const I2CBus&&) = delete; // 복사 대입 금지
+        I2CBus& operator=(const I2CBus&) = delete; // 복사 대입 금지
         
         void write_register(
-            uint8_t device_address,
-            uint8_t register_address,
-            uint8_t value
+            std::uint8_t device_address,
+            std::uint8_t register_address,
+            std::uint8_t value
         );
 
         uint8_t read_register(
-            uint8_t device_address,
-            uint8_t register_address
+            std::uint8_t device_address,
+            std::uint8_t register_address
         );
 
         void read_registers(
-            uint8_t device_address,
-            uint8_t start_address,
-            uint8_t* buffer,
-            size_t length
+            std::uint8_t device_address,
+            std::uint8_t start_address,
+            std::uint8_t* buffer,
+            std::size_t length
         );
 
     private:
         int m_fd{-1};   // member var: file descriptor
-}
+};
