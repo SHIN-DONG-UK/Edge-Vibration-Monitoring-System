e6838c74d725061e9693afd3c9aa64d1cfda8a23
SHIN-DONG-UK <ehd2610@naver.com>
2026-07-18 17:03:15 +0900

    feat: add combine bytes function

--- edge/include/driver/Adxl345Driver.hpp
+++ edge/include/driver/Adxl345Driver.hpp
@@ -1,17 +1,23 @@
 // ADXL345를 제어하는 인터페이스 드라이버
+#include "I2CBus.hpp"
+
 #include <cstdint>
 #include <string>
+#include <array>
 
 // 가속도 데이터 형식
-struct Acceleration{
+struct Acceleration
+{
     double x{};
     double y{};
     double z{};
 };
 
-class Adxl345Driver {
+class Adxl345Driver
+{
 public:
-    enum class OutputDataRate : std::uint8_t {
+    enum class OutputDataRate : std::uint8_t 
+    {
         Hz0_10 = 0x00,  // 0.10 Hz
         Hz0_20 = 0x01,  // 0.20 Hz
         Hz0_39 = 0x02,  // 0.39 Hz
@@ -26,14 +32,16 @@ public:
         Hz200  = 0x0B,  // 200 Hz
     };
 
-    enum class Range : std::uint8_t {
+    enum class Range : std::uint8_t
+    {
         G2  = 0x00,     // +-2 g
         G4  = 0x01,     // +-4 g
         G8  = 0x02,     // +-8 g
         G16 = 0x03      // +-16 g
     };
     
-    struct Config{
+    struct Config
+    {
         OutputDataRate odr{OutputDataRate::Hz200};
         Range range{Range::G4};
         bool fullResolution{true};
@@ -42,16 +50,21 @@ public:
     // 생성자: ADXL345 장치 초기화 및 측정 시작
     Adxl345Driver(
         const std::string& device_path,
-        Config& config
+        const Config& config
     );
 
     // 소멸자: 자원 반납
     ~Adxl345Driver();
 
+    Adxl345Driver(const Adxl345Driver&) = delete;               // 복사 생성자 삭제
+    Adxl345Driver& operator=(const Adxl345Driver&) = delete;    // 복사 대입 생성자 삭제
+
     // read acceleration data
     Acceleration read_acceleration();
 
 private:
+    static constexpr std::uint8_t DEVICE_ADDRESS  = 0x53;
+
     static constexpr std::uint8_t REG_DEVID       = 0x00;
     static constexpr std::uint8_t REG_BW_RATE     = 0x2C;
     static constexpr std::uint8_t REG_POWER_CTL   = 0x2D;
@@ -70,5 +83,9 @@ private:
     void set_standby_mode();
 
     // util function
-    Acceleration convert_raw_value();
+    Acceleration convert_raw_value(const std::array<uint8_t, 6>& raw_arr) const;
+    static std::int16_t combine_bytes(std::uint8_t low, std::uint8_t high);
+    
+    I2CBus m_bus;
+    Config m_config;
 };
